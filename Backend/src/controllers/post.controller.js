0f00247b3da120bfa28c8c535d6b5c7e375223a3
Pritam Singh <pritamsingh4092005@gmail.com>
2026-08-05 00:53:47 +0530

    refactor: organize backend into auth, user,and post modules

---
+++ Backend/src/controllers/post.controller.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+/**
+ * Post management
+ * 
+ * post /posts - create a new post
+ * get /posts - get all posts
+ * get /posts/:id - get a post by id
+ * patch /posts/:id - update a post by id
+ * delete /posts/:id - delete a post by id
+ */
---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** these are the routes for user related operations
+ * get /users/me
+ * patch /users/me
+ * patch /users/me/password
+ * delete /users/me
+ */
